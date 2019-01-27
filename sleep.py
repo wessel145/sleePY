5a60f48e317b7a60e998c580362caac0747abbaa
Wessel <wesselhissink@gmail.com>
2019-01-27 20:56:57 +0100

    formatting

--- sleep.py
+++ sleep.py
@@ -16,13 +16,13 @@ def check_drive_status():
     for drive in drives:
         result = str(subprocess.check_output(['hdparm', '-C', drive]))
         if "standby" in result:
-            print(drive, " standby")
+            print(drive, " \tstandby")
         else:
-            print(drive, " active")
+            print(drive, " \tactive")
 
 
 def check_cpu_status():
-    print(psutil.cpu_percent(interval=0.3))
+    print("cpu: \t", psutil.cpu_percent(interval=0.3), "\t%")
 
 
 def check_network_status():
@@ -35,7 +35,9 @@ def check_network_status():
     tx_speed = int((send2 - send1) / 1000.0)
     rx_speed = int((recv2 - recv1) / 1000.0)
 
-    print("tx: ", tx_speed, "\tkbps")
-    print("rx: ", rx_speed, "\tkbps")
+    print("tx: \t", tx_speed, "\tkbps")
+    print("rx: \t", rx_speed, "\tkbps")
 
 check_drive_status()
+check_cpu_status()
+check_network_status()
